544bd9a85a6e973d90d5651b85152cd401d27cda
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:55:07 +0100

    Update konference_recenzenti.php

--- konference_recenzenti.php
+++ konference_recenzenti.php
@@ -1,21 +1,14 @@
 <html>
 <head>
-    <title></title>
-    <meta name="generator" content="Bluefish 2.2.9" >
-    <meta name="author" content="Student" >
-    <meta name="date" content="2018-05-30T11:22:42+0100" >
-    <meta name="copyright" content="">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
     <meta http-equiv="content-style-type" content="text/css">
-    <meta http-equiv="expires" content="0">
     <title>Konference</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
+<!--Výpis uživatelského jména přihlášeného uživatele-->
 <?php
 session_start();
 if(isset($_SESSION['jmeno'])){
@@ -25,12 +18,13 @@ if(isset($_SESSION['jmeno'])){
 
 </div>';}
 else{
-    echo '<div class="prihlaseni">
-    <a href="prihlaseni.php">Přihlásit se</a><br>
-</div>';
+    header('Location: index.php');
 }
 ?>
-<h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1>
+
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
@@ -43,28 +37,38 @@ else{
     <br>
     <div class="login">
         <?php
+        //Udáje od databáze, do které se chceme připojit
         $servername="localhost";
         $username="root";
         $password="";
         $dbname="web_sp";
         $conn=mysqli_connect($servername,$username,$password,$dbname);
+        //Zajištění že se budou data načítat i v češtině
+        $conn->query('set character_set_client=utf8');
+        $conn->query('set character_set_connection=utf8');
+        $conn->query('set character_set_results=utf8');
+        $conn->query('set character_set_server=utf8');
         if ($conn->connect_error) {
             die("Chyba: " . $conn->connect_error);
         }
 
+        //Vybrání všech položek z databáze konferencí pokud jsou schválené
+        $sql = "SELECT * FROM konference WHERE stav='schvaleno'";
+            $resul = $conn->query($sql);
 
-        $prikaz = "SELECT nazev FROM konference WHERE stav='schvaleno'";
-        $resul = $conn->query($prikaz);
+            if ($resul->num_rows > 0) {
+                // output data of each row
+                while($data = $resul->fetch_assoc()) {
+                    //Výpis konferencí
+                    echo "<form method='POST' action='vypsat_konferenci.php'>";
+                    echo  '<input type="hidden" name="id" value="'.$data["id"].'"><input type="submit" class="btn btn-light" name="nazev" style="width:500px" value="'.$data["nazev"].'" ><br><br>' ;
 
-        echo "<form method='POST' action='vypsat_konferenci.php'>";
-        if ($resul->num_rows > 0) {
-            // output data of each row
-            while($data = $resul->fetch_assoc()) {
-        echo  '<input type="submit" name="nazev" style="width:500px" value="'.$data["nazev"].'" ><br><br>' ;
-
-   }}
-        echo "</form>"; ?>
+                    echo "</form>";
+                }}
+        ?>
     </div>
+</center>
+<!--Script pro obarvení řádků v tabulce-->
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <script>
         $("tr:odd").addClass("barva").css("background-color","#151515");
