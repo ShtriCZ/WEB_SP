98e4341bd997e84a1612b2d895037ef765649cc0
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:56:48 +0100

    Update pridat_konferenci.php

--- pridat_konferenci.php
+++ pridat_konferenci.php
@@ -1,32 +1,31 @@
 <html>
 <head>
-<title></title>
-<meta name="generator" content="Bluefish 2.2.9" >
-<meta name="author" content="Student" >
-<meta name="date" content="2018-05-30T11:22:42+0100" >
-<meta name="copyright" content="">
-<meta name="keywords" content="">
-<meta name="description" content="">
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
-<meta http-equiv="expires" content="0">
     <title>Přidat konferenci</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link rel="stylesheet" href="styl.css" type="text/css">
-    <script type="text/javascript" src="ckeditor4/ckeditor/ckeditor.js"></script>
+    <script type="text/javascript" src="ckeditor4/ckeditor/ckeditor.js" charset="utf-8"></script>
 </head>
 <body>
-<div class="prihlasen">
-    <?php
-    session_start();
-    echo 'Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+<!--Výpis uživatelského jména přihlášeného uživatele-->
+<?php
+session_start();
+if(isset($_SESSION['jmeno'])){
+    echo '
+<div class="prihlasen">Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+    echo '<br><a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
 
-    ?>
-    <br>
-    <a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
-</div>
-<h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1>
+</div>';}
+else{
+    header('Location: index.php');
+}
+?>
+
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
@@ -36,30 +35,26 @@
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="pridat_konferenci.php" class="text-success">PŘIDAT KONFERENCI</a></td>
         </table>
     </div>
+</center>
     <br>
     <div class="login">
         <h2>Vkládání nové konference:</h2>
         <table cellpadding="5px">
-            <form method="POST" name="form" action="ulozit_konferenci.php" onSubmit="getPath();">
+            <form method="POST" action="ulozit_konferenci.php">
                 <tr><td>Nazev: </td><td><input type="text" name="nazev" ></td></tr>
                 <tr><td> Autoři: </td><td><input type="text" name="autor"></td></tr>
-                <tr><td style="position: relative; bottom: 120px; "> Text: </td><td><textarea name="text" style="width: 500px; height: 250px; resize: none" class="ckeditor"></textarea></td></tr>
-                <tr><td> PDF: </td><td><input type="file" name="pdf" id="pdf" accept="application/pdf"></td></tr>
-                <tr><td><input type="submit" value="Odeslat" onclick="getPath()"></td></tr>
+                <tr><td style="position: relative; bottom: 120px; "> Abstrakt: </td><td><textarea name="text" style="width: 500px; height: 250px; resize: none" class="ckeditor" id="editor"></textarea></td></tr>
+                <tr><td> PDF: </td><td><input type="file" name="pdf" accept="application/pdf"></td></tr>
+                <tr><td><input type="submit" value="Odeslat"></td></tr>
             </form>
         </table>
     </div>
-
-    <script language="javascript" type="text/javascript">
-        function getPath() {
-            var Form = document.forms['form'];
-            var inputName = Form.elements['pdf'].value;
-
-            var imgPath = inputName;
-            document.write(imgPath);
-            Form.elements['pdf'].value = imgPath;
-
-        }
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
+            } );
     </script>
 </body>
 </html>
