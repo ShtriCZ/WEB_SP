f8b7d1e3e1ed0121885cb0fecadca323514f24b2
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:59:28 +0100

    Update ulozit_uzivatele.php

--- ulozit_uzivatele.php
+++ ulozit_uzivatele.php
@@ -1,16 +1,8 @@
 <html>
 <head>
-<title></title>
-<meta name="generator" content="Bluefish 2.2.9" >
-<meta name="author" content="Student" >
-<meta name="date" content="2018-05-30T11:22:42+0100" >
-<meta name="copyright" content="">
-<meta name="keywords" content="">
-<meta name="description" content="">
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
-<meta http-equiv="expires" content="0">
     <title>Uložit uživatele</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link rel="stylesheet" href="styl.css" type="text/css">
@@ -20,7 +12,9 @@
     <p class="text-success"><a href="prihlaseni.php" style="text-decoration: none;color:#007901;">Přihlásit se</a></p> <br>
 </div>
 
-<h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1>
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
@@ -29,7 +23,7 @@
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
         </table>
     </div>
-
+</center>
     <br>
 <div class="login">
 <?php
@@ -39,11 +33,14 @@ $prijmeni=htmlspecialchars($_POST['prijmeni']);
 $email=htmlspecialchars($_POST['email']);
 $uzjmeno=htmlspecialchars($_POST['uzjmeno']);
 $heslo=md5($_POST['heslo']);
+
+//Udáje od databáze, do které se chceme připojit
 $servername="localhost";
 $username="root";
 $password="";
 $dbname="WEB_SP";
 $conn = new mysqli($servername, $username, $password, $dbname);
+//Zajištění že se budou data ukládat i v češtině
 $conn->query('set character_set_client=utf8');
 $conn->query('set character_set_connection=utf8');
 $conn->query('set character_set_results=utf8');
