f35314ca886b7447933c129f8208e56a4f661d02
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 17:00:02 +0100

    Update zmenit_roli.php

--- zmenit_roli.php
+++ zmenit_roli.php
@@ -1,46 +1,57 @@
 <html>
 <head>
-    <title></title>
-    <meta name="generator" content="Bluefish 2.2.9" >
-    <meta name="author" content="Student" >
-    <meta name="date" content="2018-05-30T11:22:42+0100" >
-    <meta name="copyright" content="">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
     <meta http-equiv="content-style-type" content="text/css">
-    <meta http-equiv="expires" content="0">
     <title>Změnit roli uživatele</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
-<div class="prihlasen">
-    <?php
-    session_start();
-    echo 'Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+<!--Výpis uživatelského jména přihlášeného uživatele-->
+<?php
+session_start();
+if(isset($_SESSION['jmeno'])){
+    echo '
+<div class="prihlasen">Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+    echo '<br><a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
 
-    ?>
-    <br>
-    <a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
-</div>
-<h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1>
+</div>';}
+else{
+    header('Location: index.php');
+}
+?>
+
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
             <td></td>
-            <td style=" padding: 5px;"><a href="about.php" class="text-success">O STRÁNKÁCH</a></td>
+            <td style=" padding: 5px;"><a href="admin_menu.php" class="text-success">O STRÁNKÁCH</a></td>
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
                     <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>
                     <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>
-            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT RECENZI</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT KONFERENCI</a></td>
         </table>
     </div>
+</center>
     <br>
     <div class="login">
         <?php
         include_once("connect.php");
+        //Udáje od databáze, do které se chceme připojit
+        $servername="localhost";
+        $username="root";
+        $password="";
+        $dbname="web_sp";
+        $conn=mysqli_connect($servername,$username,$password,$dbname);
+        //Zajištění že se budou data ukládat i v češtině
+        $conn->query('set character_set_client=utf8');
+        $conn->query('set character_set_connection=utf8');
+        $conn->query('set character_set_results=utf8');
+        $conn->query('set character_set_server=utf8');
         $uzjmeno=htmlspecialchars($_POST['uzjmeno']);
         if(isset($_POST['role'])){
         $role=htmlspecialchars($_POST['role']);
@@ -48,11 +59,10 @@
         if ($conn->connect_error) {
             die("Chyba: " . $conn->connect_error);
         }
-
         if($role=="Změnit roli autora"){
             $prikaz = "SELECT * FROM autori WHERE uzjmeno='$uzjmeno'";
+
             $resul = $conn->query($prikaz);
-            echo "<br><table  cellspacing='5' border='2'>";
             if ($resul->num_rows > 0) {
                 // output data of each row
                 while($data = $resul->fetch_assoc()) {
