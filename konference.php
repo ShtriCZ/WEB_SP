4aacb231241da2aaf3bee363704876bb4a9f5d42
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:54:18 +0100

    Update konference.php

--- konference.php
+++ konference.php
@@ -1,21 +1,14 @@
 <html>
 <head>
-    <title></title>
-    <meta name="generator" content="Bluefish 2.2.9" >
-    <meta name="author" content="Student" >
-    <meta name="date" content="2018-05-30T11:22:42+0100" >
-    <meta name="copyright" content="">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
     <meta http-equiv="content-style-type" content="text/css">
-    <meta http-equiv="expires" content="0">
     <title>Konference</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
+<!--zjištění uživatelského jména uživatele a jeho vypsání, pokud není uživatel přihlášen je vypsán odkaz pro přihlášení-->
 <?php
 session_start();
 if(isset($_SESSION['jmeno'])){
@@ -30,60 +23,64 @@ else{
 </div>';
 }
 ?>
-<h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1>
+
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky podle toho zda je uživatel přihlášen jako admin nebo není přihlášen vůbec-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
             <td></td>
-            <td style=" padding: 5px;"><a href="about.php" class="text-success">O STRÁNKÁCH</a></td>
-            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
             <?php
+            //kontrola zda je někdo přihlášen
             if(isset($_SESSION['jmeno'])){
                 if($_SESSION['jmeno']=="admin"){
-                    echo('<td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>');
+                    echo('<td style=" padding: 5px;"><a href="admin_menu.php" class="text-success">O STRÁNKÁCH</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td><td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>');
                     echo('<td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>');
-                    echo ('<td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT RECENZI</a></td>');
+                    echo ('<td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT KONFERENCI</a></td>');
                 }}
+            else{
+                echo '
+            <td style=" padding: 5px;"><a href="autori_menu.php" class="text-success">O STRÁNKÁCH</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>';
+            }
             ?>
         </table>
     </div>
     <br>
     <div class="login">
         <?php
+        //Udáje od databáze, do které se chceme připojit
         $servername="localhost";
         $username="root";
         $password="";
         $dbname="web_sp";
         $conn=mysqli_connect($servername,$username,$password,$dbname);
+        //Zajištění že se budou data načítat i v češtině
+        $conn->query('set character_set_client=utf8');
+        $conn->query('set character_set_connection=utf8');
+        $conn->query('set character_set_results=utf8');
+        $conn->query('set character_set_server=utf8');
+
+        //Chyba pokud neexistuje připojení k databázi
         if ($conn->connect_error) {
             die("Chyba: " . $conn->connect_error);
         }
+        //Vybrání všech položek z databáze konferencí pokud jsou schválené
+        $sql = "SELECT * FROM konference WHERE stav='schvaleno'";
+       $resul = $conn->query($sql);
 
-
-        $db_connection = new PDO('mysql:host=localhost;dbname=WEB_SP', 'root', '');
-        $sql = "SELECT nazev FROM konference WHERE stav='schvaleno'";
-        $query = $db_connection->prepare($sql);
-        $query->bindParam(':id', $id);
-        $query->execute();
-        //getting the result
-        $query->setFetchMode(PDO::FETCH_ASSOC);
-        $result = $query->fetchColumn();
-        print(htmlentities($result));
-       /** $resul = $conn->query($prikaz);
-
-        echo "<form method='POST' action='vypsat_konferenci.php'>";
         if ($resul->num_rows > 0) {
-            // output data of each row
+            //Výpis konferencí
             while($data = $resul->fetch_assoc()) {
-        echo  '<input type="submit" name="nazev" style="width:500px" value="'.$data["nazev"].'" ><br><br>' ;
+                echo "<form method='POST' action='vypsat_konferenci.php'>";
+        echo  '<input type="hidden" name="id" value="'.$data["id"].'"><input type="submit" class="btn btn-light" name="nazev" style="width:500px" value="'.$data["nazev"].'" ><br><br>' ;
 
+                echo "</form>";
    }}
-        echo "</form>"; */
         ?>
     </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <script>
-        $("tr:odd").addClass("barva").css("background-color","#151515");
-    </script>
+</center>
 </body>
 </html>
