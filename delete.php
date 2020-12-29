0318cf700b8afcd5cb9bb0d9ad9425c19f6d0fa7
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:53:06 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,7 +3,18 @@
     unset($conn);
     include_once("connect.php");
     $id=$_POST["id"];
-    echo $id.'<br>';
+
+    //Udáje od databáze, do které se chceme připojits
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="web_sp";
+    $conn=mysqli_connect($servername,$username,$password,$dbname);
+    //Zajištění že se budou údaje načítat i v češtině
+    $conn->query('set character_set_client=utf8');
+    $conn->query('set character_set_connection=utf8');
+    $conn->query('set character_set_results=utf8');
+    $conn->query('set character_set_server=utf8');
     $deleting = mysqli_query($conn, "DELETE FROM konference WHERE id=$id") or die(mysql_error());
     header('Location: moje_konference.php');
 
