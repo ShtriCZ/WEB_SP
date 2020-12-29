a7e9bcce886ea717d1868f6d639b8b276816d2dc
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:52:47 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,18 @@
-<?php $servername="localhost";
+<?php
+//Udáje od databáze, do které se chceme připojit
+$servername="localhost";
 $username="root";
 $password="";
 $dbname="WEB_SP";
 $conn=mysqli_connect($servername,$username,$password,$dbname);
     if(!$conn){ die("chyba " . mysqli_connect_error());
 }
+//Zajištění že se budou údaje úkladat i v češtině
 $conn->query('set character_set_client=utf8');
 $conn->query('set character_set_connection=utf8');
 $conn->query('set character_set_results=utf8');
 $conn->query('set character_set_server=utf8');
+//Vytvoření tabulky autoři pokud neexistuje
 $autori = "CREATE TABLE IF NOT EXISTS `autori`(
 id					int (5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 jmeno		      	varchar(40) NOT NULL, 
@@ -24,6 +28,7 @@ else { 	echo "chyba" . mysqli_error($conn); }
 
     if(!$conn){ die("chyba " . mysqli_connect_error());
 }
+//Vytvoření tabulky recenzenti pokud neexistuje
 $recenzenti = "CREATE TABLE IF NOT EXISTS `recenzenti`(
 id					int (5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 jmeno		      	varchar(40) NOT NULL, 
@@ -39,7 +44,7 @@ else { 	echo "chyba" . mysqli_error($conn); }
 
 if(!$conn){ die("chyba " . mysqli_connect_error());
 }
-
+//Vytvoření tabulky konference pokud neexistuje
 $konference = "CREATE TABLE IF NOT EXISTS `konference`(
 id					int (5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nazev		      	varchar(40) NOT NULL,
