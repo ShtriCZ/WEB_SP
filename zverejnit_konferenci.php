393797ee8f1adbbe305f7cf42009003247e0f090
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 17:00:14 +0100

    Update zverejnit_konferenci.php

--- zverejnit_konferenci.php
+++ zverejnit_konferenci.php
@@ -1,21 +1,14 @@
 <html>
 <head>
-    <title></title>
-    <meta name="generator" content="Bluefish 2.2.9" >
-    <meta name="author" content="Student" >
-    <meta name="date" content="2018-05-30T11:22:42+0100" >
-    <meta name="copyright" content="">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
     <meta http-equiv="content-style-type" content="text/css">
-    <meta http-equiv="expires" content="0">
     <title>Zveřejnit konferenci</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
+<!--Výpis uživatelského jména přihlášeného uživatele-->
 <?php
 session_start();
 if(isset($_SESSION['jmeno'])){
@@ -25,43 +18,44 @@ if(isset($_SESSION['jmeno'])){
 
 </div>';}
 else{
-    echo '<div class="prihlaseni">
-    <a href="prihlaseni.php">Přihlásit se</a><br>
-</div>';
+    header('Location: index.php');
 }
 ?>
-<h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1>
+
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
-            <td></td>
-            <td style=" padding: 5px;"><a href="about.php" class="text-success">O STRÁNKÁCH</a></td>
-            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
-            <?php
-            if(isset($_SESSION['jmeno'])){
-                if($_SESSION['jmeno']=="admin"){
-                    echo('<td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>');
-                    echo('<td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>');
-                    echo ('<td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT RECENZI</a></td>');
-                }}
-            ?>
+                    <td style=" padding: 5px;"><a href="admin_menu.php" class="text-success">O STRÁNKÁCH</a></td>
+                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
+                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>
+                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>
+                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT KONFERENCI</a></td>
         </table>
     </div>
+</center>
     <br>
     <div class="login">
-        <a class="btn" href="schvalit.php"></a>
         <?php
+        //Udáje od databáze, do které se chceme připojit
         $servername="localhost";
         $username="root";
         $password="";
         $dbname="web_sp";
         $conn=mysqli_connect($servername,$username,$password,$dbname);
+        //Zajištění že se budou data načítat i v češtině
+        $conn->query('set character_set_client=utf8');
+        $conn->query('set character_set_connection=utf8');
+        $conn->query('set character_set_results=utf8');
+        $conn->query('set character_set_server=utf8');
         if ($conn->connect_error) {
             die("Chyba: " . $conn->connect_error);
         }
 
 
-        $prikaz = "SELECT nazev FROM konference WHERE NOT 'schvaleno'";
+        $prikaz = "SELECT nazev FROM konference WHERE stav LIKE 'ohodnoceno'";
         $resul = $conn->query($prikaz);
         echo "<br><table  cellspacing='10' border='1' style='text-align: center'>";
         if ($resul->num_rows > 0) {
