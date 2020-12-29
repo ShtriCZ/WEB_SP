da97a895027791122fbb1a5d358076a370b6b714
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:55:31 +0100

    Update moje_konference.php

--- moje_konference.php
+++ moje_konference.php
@@ -1,31 +1,30 @@
 <html>
 <head>
-<title></title>
-<meta name="generator" content="Bluefish 2.2.9" >
-<meta name="author" content="Student" >
-<meta name="date" content="2018-05-30T11:22:42+0100" >
-<meta name="copyright" content="">
-<meta name="keywords" content="">
-<meta name="description" content="">
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
-<meta http-equiv="expires" content="0">
     <title>Moje konference</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
-<div class="prihlasen">
-    <?php
-    session_start();
-    echo 'Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+<!--Výpis uživatelského jména přihlášeného uživatele-->
+<?php
+session_start();
+if(isset($_SESSION['jmeno'])){
+    echo '
+<div class="prihlasen">Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+    echo '<br><a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
 
-    ?>
-    <br>
-    <a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
-</div>
-<h1 style="position: fixed; top:1%; left: 1%" >WEB KONFERENCE</h1>
+</div>';}
+else{
+    header('Location: index.php');
+}
+?>
+
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
@@ -35,31 +34,43 @@
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="pridat_konferenci.php" class="text-success">PŘIDAT KONFERENCI</a></td>
         </table>
     </div>
+</center>
     <br>
     <div class="login">
 
 
         <?php
+        //Udáje od databáze, do které se chceme připojit
         $servername="localhost";
         $username="root";
         $password="";
         $dbname="web_sp";
         $conn=mysqli_connect($servername,$username,$password,$dbname);
+        //Zajištění že se budou data načítat i v češtině
+        $conn->query('set character_set_client=utf8');
+        $conn->query('set character_set_connection=utf8');
+        $conn->query('set character_set_results=utf8');
+        $conn->query('set character_set_server=utf8');
+        //Chyba pokud neexistuje připojení k databázi
         if ($conn->connect_error) {
             die("Chyba: " . $conn->connect_error);
         }
         $jmeno = $_SESSION['jmeno'];
-        $prikaz = "SELECT * FROM konference WHERE pridal='$jmeno'";
-        $resul = $conn->query($prikaz);
+        //Načtení údajů z databáze konferencí pokud ji přidal přihlášený autor
+        $sql = "SELECT * FROM konference WHERE pridal='$jmeno'";
+
+        $resul = $conn->query($sql);
+
         echo "<br><table  cellspacing='5' border='2' style='text-align: center'>";
         if ($resul->num_rows > 0) {
-            // output data of each row
+            //Výpis dat do tabulky
             echo("<tr><td>Název:</td><td>Originalita:</td><td>Téma:</td><td>Technická kvalita:</td><td>Jazyková kvalita:</td><td>Stav:</td>");
             while($data = $resul->fetch_assoc()) {
                 echo  "<tr><td width='1%'>".$data["nazev"]."</td><td width='1%'>".$data["originalita"]."</td><td width='1%'>".$data["tema"]."</td>
                 <td width='1%'>".$data["technicka_kvalita"]."</td><td width='1%'>".$data["jazykova_kvalita"]."</td><td width='1%'>".$data["stav"]."</td>";
                 echo "<form method='POST' action='delete.php'>";
 
+                //Pokud konference není schválena může jí autor odstranit
                 if($data["stav"]!='schvaleno'){
                    echo "<td width='1%'><input type='hidden' name='id' value='". $data["id"] . "'> <button type='submit' class='btn btn-danger'>Odstranit
         </button></td>";
@@ -69,6 +80,7 @@
         echo "</table>";
         ?>
     </div>
+<!--Script pro obarvení řádků v tabulce-->
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <script>
         $("tr:odd").addClass("barva").css("background-color","#151515");
