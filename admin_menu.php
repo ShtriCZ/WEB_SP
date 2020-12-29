9a199ceb979adb1735568ff24d1e18273c051283
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:52:08 +0100

    Update admin_menu.php

--- admin_menu.php
+++ admin_menu.php
@@ -1,47 +1,45 @@
 <html>
 <head>
-<title></title>
-<meta name="generator" content="Bluefish 2.2.9" >
-<meta name="author" content="Student" >
-<meta name="date" content="2018-05-30T11:22:42+0100" >
-<meta name="copyright" content="">
-<meta name="keywords" content="">
-<meta name="description" content="">
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
-<meta http-equiv="expires" content="0">
     <title>Menu</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
-<div class="prihlasen">
-      <?php
-       session_start();
-       echo 'Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
-    
-       ?>
-    <br>
-    <a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a>
-    </div>
-<h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1>
+<!--Výpis uživatelského jména přihlášeného uživatele-->
+<?php
+session_start();
+if(isset($_SESSION['jmeno'])){
+    echo '
+<div class="prihlasen">Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+    echo '<br><a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
+
+</div>';}
+else{
+    header('Location: index.php');
+}
+?>
+
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
-            <td style=" padding: 5px;"><a href="about.php" class="text-success">O STRÁNKÁCH</a></td>
+            <td style=" padding: 5px;"><a href="admin_menu.php" class="text-success">O STRÁNKÁCH</a></td>
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>
-            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT RECENZI</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT KONFERENCI</a></td>
         </table>
     </div>
     <br>
+    <!--Základní informace o stránkách-->
    <div class="login">
     Vítejte admine. Nyní můžete přidávat nové uživatele a spravovat konference.
     </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</center>
 </body>
 </html>
