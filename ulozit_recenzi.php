1fef652f9f20438c4130a2f87e98784a4d54414c
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2020-12-29 16:58:46 +0100

    Update ulozit_recenzi.php

--- ulozit_recenzi.php
+++ ulozit_recenzi.php
@@ -1,32 +1,30 @@
 <html>
 <head>
-<title></title>
-<meta name="generator" content="Bluefish 2.2.9" >
-<meta name="author" content="Student" >
-<meta name="date" content="2018-05-30T11:22:42+0100" >
-<meta name="copyright" content="">
-<meta name="keywords" content="">
-<meta name="description" content="">
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
-<meta http-equiv="expires" content="0">
     <title>Uložit recenzi</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
-<div class="prihlasen">
-    <?php
-    session_start();
-    echo 'Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+<!--Výpis uživatelského jména přihlášeného uživatele-->
+<?php
+session_start();
+if(isset($_SESSION['jmeno'])){
+    echo '
+<div class="prihlasen">Jste přihlášen jako: <h10 >'.$_SESSION['jmeno'].'</h10>';
+    echo '<br><a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
 
-    ?>
-    <br>
-    <a href="odhlaseni.php" style="color: #00d408">Odhlásit se</a><br>
-</div>
+</div>';}
+else{
+    header('Location: index.php');
+}
+?>
 
-<h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1>
+<!--Nadpis stránky s odkazem na úvodní stránku-->
+<a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
+<!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
         <table cellspacing="5" border="0" cellpadding="0">
@@ -36,7 +34,7 @@
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="ohodnotit.php" class="text-success">OHODNOTIT KONFERENCI</a></td>
         </table>
     </div>
-
+</center>
     <br>
 <div class="login">
 <?php
@@ -48,11 +46,14 @@ $technicka_kvalita=htmlspecialchars($_POST['technicka_kvalita']);
 $jazykova_kvalita=htmlspecialchars($_POST['jazykova_kvalita']);
 $doporuceni=htmlspecialchars($_POST['doporuceni']);
 $poznamky=htmlspecialchars($_POST['poznamky']);
+
+//Udáje od databáze, do které se chceme připojit
 $servername="localhost";
 $username="root";
 $password="";
 $dbname="WEB_SP";
 $conn = new mysqli($servername, $username, $password, $dbname);
+//Zajištění že se budou data ukládat i v češtině
 $conn->query('set character_set_client=utf8');
 $conn->query('set character_set_connection=utf8');
 $conn->query('set character_set_results=utf8');
