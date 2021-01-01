0bd9102093190d561e80909d5bd055eeb61156f6
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2021-01-01 16:40:49 +0100

    Update sprava_konference.php

--- sprava_konference.php
+++ sprava_konference.php
@@ -30,61 +30,61 @@ else{
         <table cellspacing="5" border="0" cellpadding="0">
             <td style=" padding: 5px;"><a href="admin_menu.php" class="text-success">O STRÁNKÁCH</a></td>
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
-                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>
-                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>
-                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT KONFERENCI</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT KONFERENCI</a></td>
         </table>
     </div>
 </center>
-    <br>
-    <div class="login" style="width: 1500px;">
-        <?php
-        //Udáje od databáze, do které se chceme připojit
-        $servername="localhost";
-        $username="root";
-        $password="";
-        $dbname="web_sp";
-        $conn=mysqli_connect($servername,$username,$password,$dbname);
-        //Zajištění že se budou data načítat i v češtině
-        $conn->query('set character_set_client=utf8');
-        $conn->query('set character_set_connection=utf8');
-        $conn->query('set character_set_results=utf8');
-        $conn->query('set character_set_server=utf8');
-        if ($conn->connect_error) {
-            die("Chyba: " . $conn->connect_error);
-        }
-        //Výběr dat z konference
-        $prikaz = "SELECT * FROM konference";
-        $resul = $conn->query($prikaz);
-        echo "<br><table  cellspacing='5' border='2' style='text-align: center; '>";
-        if ($resul->num_rows > 0) {
-            //Výpis dat z databáze konference
-            echo "<tr><td>Název:</td><td>Autor:</td><td>Přidal:</td><td>PDF:</td><td>Originalita:</td><td>Téma</td><td>Technická kvalita:</td>
+<br>
+<div class="login" style="width: 1500px;">
+    <?php
+    //Udáje od databáze, do které se chceme připojit
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="web_sp";
+    $conn=mysqli_connect($servername,$username,$password,$dbname);
+    //Zajištění že se budou data načítat i v češtině
+    $conn->query('set character_set_client=utf8');
+    $conn->query('set character_set_connection=utf8');
+    $conn->query('set character_set_results=utf8');
+    $conn->query('set character_set_server=utf8');
+    if ($conn->connect_error) {
+        die("Chyba: " . $conn->connect_error);
+    }
+    //Výběr dat z konference
+    $prikaz = "SELECT * FROM konference";
+    $resul = $conn->query($prikaz);
+    echo "<br><table  cellspacing='5' border='2' style='text-align: center; '>";
+    if ($resul->num_rows > 0) {
+        //Výpis dat z databáze konference
+        echo "<tr><td>Název:</td><td>Autor:</td><td>Přidal:</td><td>PDF:</td><td>Originalita:</td><td>Téma</td><td>Technická kvalita:</td>
                 <td>Jazyková kvalita:</td><td>Doporučení:</td><td>Poznámky:</td><td>Stav:</td><td>Přidat recenzenta:</td></tr>";
-            while($data = $resul->fetch_assoc()) {
-                $nazev=$data["nazev"];
-                echo  "<tr><td width='1%'>".$data["nazev"]."</td><td>".$data["autor"]."</td><td>".$data["pridal"]."</td><td><a href='pdf/".$data["pdf"]."'>".$data["pdf"]."</a></td>
+        while($data = $resul->fetch_assoc()) {
+            $id=$data["id"];
+            echo  "<tr><td width='1%'>".$data["nazev"]."</td><td>".$data["autor"]."</td><td>".$data["pridal"]."</td><td><a style='text-decoration: none;color:#007901;' href='pdf/".$data["pdf"]."' target='blank'>".$data["pdf"]."</a></td>
                         <td>".$data["originalita"]."</td><td>".$data["tema"]."</td><td>".$data["technicka_kvalita"]."</td>
-                        <td>".$data["jazykova_kvalita"]."</td><td>".$data["doporuceni"]."</td><td title='".$data["poznamky"]."'>".substr($data["poznamky"],0,10)."...</td><td>".$data["stav"]."</td>";
-        //Vybrání dat z konference a možnost přiřadit recenzenty ke konferenci
-        $prika = "SELECT * FROM recenzenti";
-        $result = $conn->query($prika);
-        echo "<td><form method='post' action='ulozit_recenzenty.php'><input type='hidden' name='nazev' value='$nazev'>";
-        if ($result->num_rows > 0) {
-            // output data of each row
-            echo " <br><select name='id' >";
-            while($dat = $result->fetch_assoc()) {
-                echo  "<option value='".$dat["id"]."'>".$dat["jmeno"] ." ". $dat["prijmeni"]."</option>";
-            }}
-        echo "<input type='submit'value='Uložit'></form></td></tr>";
-            }}
-        echo "</table>";
-        ?>
-    </div>
-    <!--Skript pro obarvení lichých řádků-->
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <script>
-        $("tr:odd").addClass("barva").css("background-color","#151515");
-    </script>
+                        <td>".$data["jazykova_kvalita"]."</td><td>".$data["doporuceni"]."</td><td title='".$data["poznamky"]."'>".mb_substr($data["poznamky"],0,10)."...</td><td>".$data["stav"]."</td>";
+            //Vybrání dat z konference a možnost přiřadit recenzenty ke konferenci
+            $prika = "SELECT * FROM recenzenti";
+            $result = $conn->query($prika);
+            echo "<td><form method='post' action='ulozit_recenzenty.php'><input type='hidden' name='id_konf' value='$id'>";
+            if ($result->num_rows > 0) {
+                // output data of each row
+                echo " <br><select name='id' >";
+                while($dat = $result->fetch_assoc()) {
+                    echo  "<option value='".$dat["id"]."'>".$dat["jmeno"] ." ". $dat["prijmeni"]."</option>";
+                }}
+            echo "<input type='submit'value='Uložit'></form></td></tr>";
+        }}
+    echo "</table>";
+    ?>
+</div>
+<!--Skript pro obarvení lichých řádků-->
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<script>
+    $("tr:odd").addClass("barva").css("background-color","#151515");
+</script>
 </body>
 </html>
