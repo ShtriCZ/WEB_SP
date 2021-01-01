465b003cfebb4ef0dfd7d2fce41ecdd3520b59b1
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2021-01-01 16:39:50 +0100

    Update pridat_uzivatele.php

--- pridat_uzivatele.php
+++ pridat_uzivatele.php
@@ -1,11 +1,11 @@
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
-<meta http-equiv="content-style-type" content="text/css">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+    <meta http-equiv="content-style-type" content="text/css">
     <title>Přidat uživatele</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<link rel="stylesheet" href="styl.css" type="text/css">
+    <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
 
@@ -21,21 +21,21 @@
         </table>
     </div>
 </center>
- 
-    <br>
-   <div class="login">
-       <h2>Vytvořit účet:</h2>
-       <table cellpadding="5px">
-           <!--Formulář pro vytvoření nového uživatele-->
-       <form method="POST" action="ulozit_uzivatele.php">
-           <tr><td>Jméno: </td><td><input type="text" name="jmeno" ></td></tr>
-           <tr><td> Příjmení: </td><td><input type="text" name="prijmeni"></td></tr>
-           <tr><td> Email: </td><td><input type="email" name="email"></td></tr>
-           <tr><td>Uživatelské jméno: </td><td><input type="text" name="uzjmeno"></td></tr>
-           <tr><td>Heslo: </td><td><input type="text" name="heslo" ></td></tr>
-           <tr><td><input type="submit" value="Odeslat"></td></tr>
-       </form>
-       </table>
+
+<br>
+<div class="login">
+    <h2>Vytvořit účet:</h2>
+    <table cellpadding="5px">
+        <!--Formulář pro vytvoření nového uživatele-->
+    <form method="POST" action="ulozit_uzivatele.php">
+        <tr><td>Jméno: </td><td><input type="text" name="jmeno" ></td></tr>
+        <tr><td> Příjmení: </td><td><input type="text" name="prijmeni"></td></tr>
+        <tr><td> Email: </td><td><input type="email" name="email"></td></tr>
+        <tr><td>Uživatelské jméno: </td><td><input type="text" name="uzjmeno"></td></tr>
+        <tr><td>Heslo: </td><td><input type="text" name="heslo" ></td></tr>
+        <tr><td><input type="submit" value="Odeslat"></td></tr>
+    </form>
+    </table>
 </div>
-    </body>
+ </body>
 </html>
