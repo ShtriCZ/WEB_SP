34d931d2eec30fea3bde51c4037b2ace5210e614
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2021-01-01 16:40:02 +0100

    Update prihlaseni.php

--- prihlaseni.php
+++ prihlaseni.php
@@ -1,17 +1,17 @@
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
-<meta http-equiv="content-style-type" content="text/css">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+    <meta http-equiv="content-style-type" content="text/css">
     <title>Přihlášení</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<link rel="stylesheet" href="styl.css" type="text/css">
+    <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
-    <!--Odkaz pro přihlášení-->
+<!--Odkaz pro přihlášení-->
 <div class="prihlaseni">
     <a href="prihlaseni.php" style="text-decoration: none;color:#007901;">Přihlásit se</a><br>
-    </div>
+</div>
 
 <!--Nadpis stránky s odkazem na úvodní stránku-->
 <a href="index.php"><h1 style="position: fixed; top:1%; left: 1%">WEB KONFERENCE</h1></a>
@@ -25,18 +25,18 @@
         </table>
     </div>
 </center>
-    <div class="abc">
-        <table cellpadding="5px">
-            <!--Formulář pro přihlášení-->
-            <form method="POST" action="login.php">
-                <tr><td>Uživatelské jméno:</td> <td><input type="text" name="jmeno" placeholder="Uživatelské jméno"></td></tr>
-                <tr><td>Heslo: </td><td><input type="password" name="heslo" placeholder="Heslo"></td></tr>
-                <td><button type="submit" class="btn btn-light">Přihlásit</button></td>
+<div class="abc">
+    <table cellpadding="5px">
+        <!--Formulář pro přihlášení-->
+        <form method="POST" action="login.php">
+            <tr><td>Uživatelské jméno:</td> <td><input type="text" name="jmeno" placeholder="Uživatelské jméno"></td></tr>
+            <tr><td>Heslo: </td><td><input type="password" name="heslo" placeholder="Heslo"></td></tr>
+            <td><button type="submit" class="btn btn-light">Přihlásit</button></td>
 
-            </form>
-       </table>
-        <!--Odkaz pokud chce uživatel vytvořit nový účet-->
-        <a href="pridat_uzivatele.php" style="color: #00d408">Vytvořit účet.</a>
-    </div>
+        </form>
+    </table>
+    <!--Odkaz pokud chce uživatel vytvořit nový účet-->
+    <a href="pridat_uzivatele.php" style="color: #00d408">Vytvořit účet.</a>
+</div>
 </body>
 </html>
