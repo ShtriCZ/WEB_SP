35877325ebd05cec863a177e611e84d0e9ad90de
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2021-01-01 16:41:26 +0100

    Update ulozit_konferenci.php

--- ulozit_konferenci.php
+++ ulozit_konferenci.php
@@ -1,11 +1,11 @@
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
-<meta http-equiv="content-style-type" content="text/css">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+    <meta http-equiv="content-style-type" content="text/css">
     <title>Uložit konferenci</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<link rel="stylesheet" href="styl.css" type="text/css">
+    <link rel="stylesheet" href="styl.css" type="text/css">
 </head>
 <body>
 <!--Výpis uživatelského jména přihlášeného uživatele-->
@@ -27,54 +27,54 @@ else{
 <!--Menu s odkazy na další stránky-->
 <center>
     <div class="aaa">
-            <table cellspacing="5" border="0" cellpadding="0">
-                <td style=" padding: 5px;"><a href="autori_menu.php" class="text-success">O STRÁNKÁCH</a></td>
-                <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference_autori.php" class="text-success">KONFERENCE</a></td>
-                <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="moje_konference.php" class="text-success">MOJE KONFERENCE</a></td>
-                <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="pridat_konferenci.php" class="text-success">PŘIDAT KONFERENCI</a></td>
-            </table>
+        <table cellspacing="5" border="0" cellpadding="0">
+            <td style=" padding: 5px;"><a href="autori_menu.php" class="text-success">O STRÁNKÁCH</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="moje_konference.php" class="text-success">MOJE KONFERENCE</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="pridat_konferenci.php" class="text-success">PŘIDAT KONFERENCI</a></td>
+        </table>
     </div>
-</center>
-    <br>
+<br>
 <div class="login">
-<?php
-include_once("connect.php");
+    <?php
+    include_once("connect.php");
     //Načtení dat
-$nazev=htmlspecialchars($_POST['nazev']);
-$autor=htmlspecialchars($_POST['autor']);
-$text=($_POST['text']);
-$text=trim($text);
-$text=stripslashes($text);
-$text=htmlspecialchars($text);
-$pdf=htmlspecialchars($_POST['pdf']);
-$pridal=htmlspecialchars($_SESSION['jmeno']);
-$stav="nehodnoceno";
+    $nazev=htmlspecialchars($_POST['nazev']);
+    $autor=htmlspecialchars($_POST['autor']);
+    $text=($_POST['text']);
+    $text=trim($text);
+    $text=stripslashes($text);
+    $text=htmlspecialchars($text);
+    $pdf=htmlspecialchars($_POST['pdf']);
+    $pridal=htmlspecialchars($_SESSION['jmeno']);
+    $stav="nehodnoceno";
 
-//Udáje od databáze, do které se chceme připojit
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="WEB_SP";
-$conn = new mysqli($servername, $username, $password, $dbname);
-//Zajištění že se budou data ukládat i v češtině
-$conn->query('set character_set_client=utf8');
-$conn->query('set character_set_connection=utf8');
-$conn->query('set character_set_results=utf8');
-$conn->query('set character_set_server=utf8');
+    //Udáje od databáze, do které se chceme připojit
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="WEB_SP";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Zajištění že se budou data ukládat i v češtině
+    $conn->query('set character_set_client=utf8');
+    $conn->query('set character_set_connection=utf8');
+    $conn->query('set character_set_results=utf8');
+    $conn->query('set character_set_server=utf8');
     //Uložení dat do databáze
-$sql = "INSERT INTO `konference` (nazev, autor, text, pdf, pridal, stav)
+    $sql = "INSERT INTO `konference` (nazev, autor, text, pdf, pridal, stav)
 VALUES ('$nazev', '$autor', '$text', '$pdf', '$pridal', '$stav')";
 
 
-if ($conn->query($sql) === TRUE) {
-    echo "Vytvořen nový záznam:";
-} else {
-    echo "Chyba: " . $sql . "<br>" . $conn->error;
+    if ($conn->query($sql) === TRUE) {
+        echo "Konference uložena.";
+    } else {
+        echo "Chyba: " . $sql . "<br>" . $conn->error;
 
-$conn->close();}
+        $conn->close();}
 
-?>
+    ?>
 
 </div>
+</center>
 </body>
 </html>
