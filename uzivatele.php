0fbaa318548987c0782131860b6d1afe11f62dbd
ShtriCZ <76259477+ShtriCZ@users.noreply.github.com>
2021-01-01 16:42:46 +0100

    Update uzivatele.php

--- uzivatele.php
+++ uzivatele.php
@@ -32,83 +32,83 @@ else{
             <td></td>
             <td style=" padding: 5px;"><a href="admin_menu.php" class="text-success">O STRÁNKÁCH</a></td>
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="konference.php" class="text-success">KONFERENCE</a></td>
-                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>
-                    <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="sprava_konference.php" class="text-success">SPRÁVA KONFERENCÍ</a></td>
+            <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="uzivatele.php" class="text-success">UŽIVATELÉ</a></td>
             <td style="border-left: 1px solid darkgray; padding: 5px;"><a href="zverejnit_konferenci.php" class="text-success">ZVEŘEJNIT KONFERENCI</a></td>
         </table>
     </div>
-</center>
-    <br>
-    <div class="login">
+<br>
+<div class="login">
 
-        <a href='role_uzivatele.php' style="color: #00d408">Změnit roli uživatele</a><br><br>
-        <?php
-        //Udáje od databáze, do které se chceme připojit
-        $servername="localhost";
-        $username="root";
-        $password="";
-        $dbname="web_sp";
-        $conn=mysqli_connect($servername,$username,$password,$dbname);
-        //Zajištění že se budou data načítat i v češtině
-        $conn->query('set character_set_client=utf8');
-        $conn->query('set character_set_connection=utf8');
-        $conn->query('set character_set_results=utf8');
-        $conn->query('set character_set_server=utf8');
-        //Chyba pokud není spojení s databází
-        if ($conn->connect_error) {
-            die("Chyba: " . $conn->connect_error);
-        }
-        //Tlačítko pro ukázání a skrytí autorů
-        echo("<button id='autor' class='btn btn-light'>Autoři</button><p id='autori'>");
-        //Výběr dat z databáze autorů
-        $prikaz = "SELECT * FROM autori";
-        $resul = $conn->query($prikaz);
-        echo "<br><table  cellspacing='5' border='2'>";
-        if ($resul->num_rows > 0) {
-            //Výpis dat o autorech
-            echo("<tr><td>Jméno:</td><td>Příjmení:</td><td>Email:</td><td>Uživatelské jméno:</td><td>Heslo:</td></tr>");
-            while($data = $resul->fetch_assoc()) {
-                echo  "<tr><td width='1%'>".$data["jmeno"]."</td><td width='1%'>".$data["prijmeni"]."</td><td width='1%'>".$data["email"]."</td>
+    <a href='role_uzivatele.php' style="color: #00d408">Změnit roli uživatele</a><br><br>
+    <?php
+    //Udáje od databáze, do které se chceme připojit
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="web_sp";
+    $conn=mysqli_connect($servername,$username,$password,$dbname);
+    //Zajištění že se budou data načítat i v češtině
+    $conn->query('set character_set_client=utf8');
+    $conn->query('set character_set_connection=utf8');
+    $conn->query('set character_set_results=utf8');
+    $conn->query('set character_set_server=utf8');
+    //Chyba pokud není spojení s databází
+    if ($conn->connect_error) {
+        die("Chyba: " . $conn->connect_error);
+    }
+    //Tlačítko pro ukázání a skrytí autorů
+    echo("<button id='autor' class='btn btn-light'>Autoři</button><p id='autori'>");
+    //Výběr dat z databáze autorů
+    $prikaz = "SELECT * FROM autori";
+    $resul = $conn->query($prikaz);
+    echo "<br><table  cellspacing='5' border='2'>";
+    if ($resul->num_rows > 0) {
+        //Výpis dat o autorech
+        echo("<tr><td>Jméno:</td><td>Příjmení:</td><td>Email:</td><td>Uživatelské jméno:</td><td>Heslo:</td></tr>");
+        while($data = $resul->fetch_assoc()) {
+            echo  "<tr><td width='1%'>".$data["jmeno"]."</td><td width='1%'>".$data["prijmeni"]."</td><td width='1%'>".$data["email"]."</td>
                 <td width='1%'>".$data["uzjmeno"]."</td><td width='1%'>".$data["heslo"]."</td></tr>";
-                }}
-        echo "</table></p>";
-        
-        //Tlačítko pro ukázání a skrytí recenzentů
-        echo("<br><br><button id='recenzent' class='btn btn-light'>Recenzenti</button><p id='recenzenti'>");
-        //Výběr dat z databáze recenzentů
-        $prikaz = "SELECT * FROM recenzenti";
-        $resul = $conn->query($prikaz);
-        echo "<br><table  cellspacing='10' border='1'>";
-        if ($resul->num_rows > 0) {
-            //Výpis dat o recenzentech
-            echo("<tr><td>Jméno:</td><td>Příjmení:</td><td>Email:</td><td>Uživatelské jméno:</td><td>Heslo:</td></tr>");
-            while($data = $resul->fetch_assoc()) {
-                echo  "<tr><td width='1%'>".$data["jmeno"]."</td><td width='1%'>".$data["prijmeni"]."</td><td width='1%'>".$data["email"]."</td>
+        }}
+    echo "</table></p>";
+
+    //Tlačítko pro ukázání a skrytí recenzentů
+    echo("<br><br><button id='recenzent' class='btn btn-light'>Recenzenti</button><p id='recenzenti'>");
+    //Výběr dat z databáze recenzentů
+    $prikaz = "SELECT * FROM recenzenti";
+    $resul = $conn->query($prikaz);
+    echo "<br><table  cellspacing='5' border='2'>";
+    if ($resul->num_rows > 0) {
+        //Výpis dat o recenzentech
+        echo("<tr><td>Jméno:</td><td>Příjmení:</td><td>Email:</td><td>Uživatelské jméno:</td><td>Heslo:</td></tr>");
+        while($data = $resul->fetch_assoc()) {
+            echo  "<tr><td width='1%'>".$data["jmeno"]."</td><td width='1%'>".$data["prijmeni"]."</td><td width='1%'>".$data["email"]."</td>
                 <td width='1%'>".$data["uzjmeno"]."</td><td width='1%'>".($data["heslo"])."</td></tr>";
-                }}
-        echo "</table></p>";
-        ?>
-       </div>
-    <!--Skript pro obarvení lichých řádků-->
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <script>
-        $("tr:odd").addClass("barva").css("background-color","#151515");
-    </script>
-    <!--Skripty pro skrývání tabulek-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $("#recenzent").click(function(){
-                $("#recenzenti").toggle();
-            });
+        }}
+    echo "</table></p>";
+    ?>
+</div>
+</center>
+<!--Skript pro obarvení lichých řádků-->
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<script>
+    $("tr:odd").addClass("barva").css("background-color","#151515");
+</script>
+<!--Skripty pro skrývání tabulek-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $("#recenzent").click(function(){
+            $("#recenzenti").toggle();
         });
-    </script>
-    <script>
-        $(document).ready(function(){
-            $("#autor").click(function(){
-                $("#autori").toggle();
-            });
+    });
+</script>
+<script>
+    $(document).ready(function(){
+        $("#autor").click(function(){
+            $("#autori").toggle();
         });
-    </script>
+    });
+</script>
 </body>
 </html>
